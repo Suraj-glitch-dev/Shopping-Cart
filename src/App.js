545edb55dde799e8d68fc299b3022f6ee2a78b05
Suraj <surajshankar@gmail.com>
2022-08-18 21:07:42 +0530

    react router added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import {  Switch, Route } from 'react-router-dom';
+import Products from './components/Products';
 
 function App() {
   return (
     <>
-      <Navbar/>
-      <Home/>
+      <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/products" component={Products} />
+        </Switch>
     </>
-
   );
 }
 
